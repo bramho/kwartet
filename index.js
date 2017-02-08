6f2e67112b47888452026c389682693df2ce3b49
bramho <bramhoningh16@gmail.com>
2017-02-08 10:19:35 +0100

    Added json and random pick of cards

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 var app = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var username = "Test";
+
+app.use('/public', express.static('public'));
 
 app.get('/', function(req, res) {
    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
+   socket.on('username', function(username) {
+     username = username;
+  });
+
+   socket.emit('user connected', { userConnect: username + ' connected' });
+
+   socket.on('disconnect', function(){
+    socket.emit('disconnected', { disconnected: 'A user disconnected'});
+  });
+
   socket.on('chat message', function(msg) {
+     console.log(msg);
     io.emit('chat message', msg);
   });
 });
